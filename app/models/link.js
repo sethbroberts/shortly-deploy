b611d71d765b55a47bef64c6b50217c453ee6e4d
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-11-06 00:00:14 -0500

    bug fix in link.js, passing all tests now

--- app/models/link.js
+++ app/models/link.js
@@ -2,7 +2,6 @@ var db = require('../config');
 var crypto = require('crypto');
 var mongoose = require('mongoose');
 
-
 var linkSchema = mongoose.Schema({
   visits: Number,
   link: String,
@@ -16,7 +15,7 @@ var Link = mongoose.model('Link', linkSchema);
 
 var createSha = function(url) {
   var shasum = crypto.createHash('sha1');
-  shasum.update('url');
+  shasum.update(url);
   return shasum.digest('hex').slice(0, 5);
 };
 
